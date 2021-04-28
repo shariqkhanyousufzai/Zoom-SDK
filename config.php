f81642ad54dc7c98503fbd983b5c6e06c8e2ab1d
Shariq Ali Khan <38683828+shariqkhanyousufzai@users.noreply.github.com>
2021-04-28 17:40:49 +0500

    ZOOM SDK

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+require_once 'vendor/autoload.php';
+require_once "class-db.php";
+  
+define('CLIENT_ID', 'wr5I5zZBSp2oKQFYv2Xwgg');
+define('CLIENT_SECRET', 'HcFRaZonLtgH4vtSX3B3hmA6RTZJ3uAa');
+define('REDIRECT_URI', 'https://62ceaa8bf3a9.ngrok.io/zoom/callback.php');
